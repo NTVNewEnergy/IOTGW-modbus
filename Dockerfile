d076a837426ad7fe492967fdb647e67f36b0470c
NTVNewEnergy <126950721+NTVNewEnergy@users.noreply.github.com>
2023-03-04 16:52:31 +0700

    Initialization

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+# syntax=docker/dockerfile:1
+FROM python:3.10-alpine
+ENV PYTHONUNBUFFERED=1
+RUN mkdir /etc/modbus/
+WORKDIR /etc/modbus/
+RUN apk add build-base && \
+  apk add git && \
+  git clone https://github.com/NTVNewEnergy/IOTGW-modbus.git /etc/modbus/ && \
+  pip install 'asyncio' 'pymodbus==2.5.3' 'aioredis==1.3.1' && \
+  apk del build-base linux-headers pcre-dev openssl-dev && \
+  rm -rf /var/cache/apk/*
+CMD ["python", "appConnectivity.py"]
---
+++ appConnectivity.py
@@ -0,0 +1,73 @@
+import asyncio
+from modbus import ModbusDevice
+from queueHandler import Message
+from threading import Thread
+import threading
+import functools
+import json
+from collections import OrderedDict
+import datetime
+
+mbD = []
+configPath = "/etc/modbus/deviceConfig.conf"
+msg = None
+
+def deviceInit(eventloop, sharedQueue):
+	global msg
+	def start_loop(device):
+		print('Connecting device ... from thread: ', d._ip, threading.current_thread().name)
+		device.start(on_read_register)
+	
+	def on_read_register(regSet):
+		for reg in regSet:
+			asyncio.run_coroutine_threadsafe(send2Redis(reg), eventloop)
+
+	print('Create devices ...')
+	conf = getConfig(configPath)
+	if conf is not None:
+		msgConf = conf.get("micro")
+		msg = Message(msgConf["ip"], msgConf["port"])
+		for (thingID, mbConf) in conf.get("modbustcp").items():
+			mbD.append(ModbusDevice(thingID, mbConf, sharedQueue, eventloop))
+			print('Device was created: ', mbConf.get("ip"), mbConf.get("port"), mbConf.get("unitID"))
+		for d in mbD:
+			t = Thread(target=start_loop, args=[d])
+			t.daemon = True
+			t.start()
+		print('About to run loop in mainthread')
+
+def getConfig(configPath):
+	try:
+		with open(configPath, 'r') as f:
+			config = json.load(f, object_pairs_hook=OrderedDict)
+	except ValueError:
+		config = None
+		print('No config file found or The file is invalid. Please check')
+	return config
+
+async def on_msg_queue(queue):
+	while True:
+		# Get a "work item" out of the queue.
+		data = await queue.get()
+		# Notify the queue that the "work item" has been processed.
+		queue.task_done()
+		#print('Send to Redis value {} in {}'.format(data, threading.current_thread().name))
+		#data["timeStamp"] = str(datetime.datetime.now())
+		topic = 'data/' + data["thingID"] + '/' + data["datapoint"]
+		dataString = json.dumps(data["dataValue"])
+		await msg.send_message(topic, dataString)
+
+async def send2Redis(reg):
+	await asyncio.sleep(1)
+	print('Send to Redis value {} in {}'.format(reg, threading.current_thread().name))
+
+async def main():
+	loop = asyncio.get_running_loop()
+	queue = asyncio.Queue()
+	asyncio.set_event_loop(loop)
+	deviceInit(loop, queue)
+	await msg.connect_to_redis()
+	await loop.create_task(on_msg_queue(queue))
+
+if __name__ == '__main__':
+	asyncio.run(main())
